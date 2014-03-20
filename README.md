703f1ed1d2f701a1415582d29ca0bb80136ad2f5
Mariusz Nowak <mariusz@medikoo.com>
2014-03-20 13:21:43 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,36 @@
+# bespoke-substeps
+## Substeps for [Bespoke.js](https://github.com/markdalgleish/bespoke.js) presentations
+
+Similar and initially inspired on [bespoke-bullets](https://github.com/markdalgleish/bespoke-bullets)
+Difference is that with _substeps_ you can customize order of display (not necesarry as it's in DOM), and configure steps to appear in parallel.
+
+### Usage
+
+```javascript
+bespoke.plugins.substeps = require('bespoke-substeps');
+
+bespoke.from(selector, {
+  substeps: true
+});
+```
+
+Example HTML configuration
+```html
+<section class="bespoke-slide">
+  <h1>Slide title</h1>
+  <ul>
+    <li class="substep">First step</li><!-- assigned  order: 0 -->
+    <li class="substep">Second step</li><!-- assigned order: 0.01 -->
+    <li class="substep" data-order="2">Fourth step (1)</p><!-- assigned  order: 2 -->
+    <li class="substep" data-order="1">Third step</p><!-- assigned  order: 1 -->
+    <li class="substep" data-order="2">Fourth step (2)</p><!-- assigned  order: 2 -->
+	</ul>
+</section>
+```
+
+### Installation
+#### npm
+
+In your presentation path:
+
+	$ npm install bespoke-substeps
---
+++ dom.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var toPositiveInteger = require('es5-ext/lib/Number/to-uint')
+  , memoize           = require('memoizee/lib/regular')
+
+  , forEach = Array.prototype.forEach, keys = Object.keys
+  , byNum = function (a, b) { return a - b; }
+  , getSubsteps;
+
+getSubsteps = memoize(function (element) {
+	var map = {}, defaultOrder = -0.01;
+	forEach.call(element.querySelectorAll('.substep'), function (el) {
+		var order = Number(el.dataset.order);
+		if (isNaN(order)) order = (defaultOrder += 0.01);
+		if (!map[order]) map[order] = [el];
+		else map[order].push(el);
+	});
+	return keys(map).sort(byNum).map(function (order) { return map[order]; });
+});
+
+module.exports = function (deck/*, options*/) {
+	var activeSubstep = 0;
+
+	deck.on('activate', function (e) {
+		var substep = toPositiveInteger(e.substep);
+		getSubsteps(e.slide).forEach(function (els, index) {
+			var visible = substep > index;
+			els.forEach(function (el) {
+				el.classList[visible ? 'add' : 'remove']('active');
+				el.classList[visible ? 'remove' : 'add']('inactive');
+			});
+		});
+		activeSubstep = substep;
+	});
+
+	deck.on('next', function () {
+		var activeSlide = deck.slide();
+		if (!getSubsteps(deck.slides[activeSlide])[activeSubstep]) return;
+		deck.slide(activeSlide, { substep: activeSubstep + 1 });
+		return false;
+	});
+
+	deck.on('prev', function () {
+		var activeSlide = deck.slide();
+		if (!activeSlide) return;
+		if (!activeSubstep) {
+			--activeSlide;
+			deck.slide(activeSlide, {
+				substep: getSubsteps(deck.slides[activeSlide]).length
+			});
+		} else {
+			deck.slide(activeSlide, { substep: activeSubstep - 1 });
+		}
+		return false;
+	});
+};
